b59cce449425f930c6591f32b7204ab0cb450d83
John Paulo Javier <j.javier.556868@umindanao.edu.ph>
2026-02-03 09:56:41 +0800

    .

--- IMS.Application/Form1.cs
+++ IMS.Application/Form1.cs
@@ -1,10 +1,19 @@
+using IMS.Infrastracture.Data;
+
 namespace IMS.Application
 {
     public partial class Form1 : Form
     {
-        public Form1()
-        {
+
+        private AppDbContext _context = new AppDbContext();
+        public Form1() { 
+
             InitializeComponent();
+            LoadPrograms();
+        }
+
+        private void LoadPrograms() {
+           dataGridView1.DataSource = _context.Programs.ToList();
         }
     }
 }
